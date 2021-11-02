2a4446d34ee244af8352309211bfeea1794384ad
Dan <khazifire@gmail.com>
2021-11-02 23:00:49 +0700

    updated healdine section with some animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        'pulse':'7s',
+      }
+    },
   },
   variants: {
     extend: {},
