ba44f1a0e66f87c30fd614bfd2f282bb3d850ce3
Dan <khazifire@gmail.com>
2021-11-05 14:12:30 +0700

    updated paddings  structure, headline section and about me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [
     './public/**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      primary:'#05203D',
+      secondary:'#F4B943',
+      tertiary: '#DCDCDB',
+      other:'#FAF7F1',
+
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      blue: colors.blue,
+      indigo: colors.indigo,
+    },
     extend: {
       animation:{
         'pulse':'7s',
@@ -13,5 +30,15 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({addUtilities}) {
+      const extendUnderline = {
+          '.underline': {
+              'textDecoration': 'underline',
+              'text-decoration-color': '#FBBF24',
+          },
+      }
+      addUtilities(extendUnderline)
+      }
+  ]
 }
