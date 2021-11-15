bd3ce328afd0dce3d080dcf22fd7c67873178b3e
Dan <khazifire@gmail.com>
2021-11-15 08:46:51 +0700

    updated the colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     colors:{
       primary:'#0E1117', /* black shade 0e0e10 */
-      secondary:'#FBBF24', /* yellow shade */
+      secondary:'#FBBF24', /* yellow shade FBBF24*/
+      hover:'#e4db5c',
       other:'#E5E7EB', /* white shade */
       tertiary: '#F8FAFC',
    
